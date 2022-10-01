6d27ca618a3a4e30cd4d7ad85cda6b5d227a1ecb
Victoria <victoriahope5683@gmail.com>
2022-10-01 10:20:14 -0500

    fixed for loop exercise

--- loops/forLoopsExercises.js
+++ loops/forLoopsExercises.js
@@ -1,46 +1,42 @@
 const days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 const myFriendsSchedule = {
-    sunday: true,
-    monday: true,
-    tuesday: false,
-    wednesday: false,
-    thursday: true,
-    friday: true,
-    saturday: false,
+    Sunday: true,
+    Monday: true,
+    Tuesday: false,
+    Wednesday: false,
+    Thursday: true,
+    Friday: true,
+    Saturday: true,
 }
 
 const myMoneySchedule = {
-    sunday: 50,
-    monday: 5,
-    tuesday: 30,
-    wednesday: 10,
-    thursday: 10,
-    friday: 10,
-    saturday: 30,
+    Sunday: 50,
+    Monday: 5,
+    Tuesday: 30,
+    Wednesday: 10,
+    Thursday: 10,
+    Friday: 10,
+    Saturday: 30,
 }
 
-function canWeGoToTheMovies() {
+function canWeGoToTheMovies(){
     let whatDaysCanWeGoToTheMovies = []
     let dayOfWeek = ""
     for (let i = 0; i < days_of_week.length; i++) {
         dayOfWeek = `${days_of_week[i]}`
-        //console.log(dayOfWeek);
-        console.log(myFriendsSchedule[i])
-        console.log(myFriendsSchedule[dayOfWeek])
-
-        //testing: when i try to reference the objects it comes back undefined. meaning all the loops return the last else condition
-        if(myFriendsSchedule[dayOfWeek] == true && myMoneySchedule[dayOfWeek] >= 30){
+        //console.log(dayOfWeek)
+        if (myFriendsSchedule[dayOfWeek] && myMoneySchedule[dayOfWeek] >= 30) {
             whatDaysCanWeGoToTheMovies.push(dayOfWeek);
             console.log(`On ${dayOfWeek}? Yes, lets go to the movies!!`)
-        }else if(myFriendsSchedule[dayOfWeek] == false && myMoneySchedule[dayOfWeek] < 30){
+        } else if (!myFriendsSchedule[dayOfWeek] && myMoneySchedule[dayOfWeek] < 30) {
             console.log(`On ${dayOfWeek}? We can't go to the movies :(`)
-        }else{
+        } else {
             console.log(`On ${dayOfWeek}?  One of us can go to the movies`)
         }
-        
+        //console.log(myFriendsSchedule[dayOfWeek])
     }
-    //console.log(`These are the days we can go to the movies: ${whatDaysCanWeGoToTheMovies.join(", ")}`)
+    console.log(`We can go to the movies together on ${whatDaysCanWeGoToTheMovies.join(", ")}`);
 
 }
 
